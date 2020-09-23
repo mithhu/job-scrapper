81652d7fdd488289fe525223a19a24b040f66156
mithhu <mahamudul.hasan@evaly.com.bd>
2020-09-23 16:01:17 +0600

    dot env

--- sheet.js
+++ sheet.js
@@ -1,19 +1,20 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 require("dotenv").config();
+module.exports;
 
 module.exports = class Sheet {
   constructor() {
     // spreadsheet key is the long id in the sheets URL
-    this.doc = new GoogleSpreadsheet(process.env.GOOGLE_CLOUD_KEY);
+    this.doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_KEY);
   }
 
   async load() {
-    await this.doc.useServiceAccountAuth(require("./credentials.json"));
+    await this.doc.useServiceAccountAuth(credentials);
     await this.doc.loadInfo(); // loads document properties and worksheets}`
   }
 
-  async addRows(rows) {
-    const sheet = this.doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-    await sheet.addRows(rows);
-  }
+  // async addRows(rows) {
+  //   const sheet = this.doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
+  //   await sheet.addRows(rows);
+  // }
 };
